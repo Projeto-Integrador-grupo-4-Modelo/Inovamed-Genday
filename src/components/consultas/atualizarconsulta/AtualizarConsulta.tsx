19d081ef5af5787d989f1131eb67715ae4d8c940
Vitor-C-Souza <vitor_souza97@hotmail.com>
2025-03-18 15:26:51 -0300

    Arrumado o erro

--- src/components/consultas/atualizarconsulta/AtualizarConsulta.tsx
+++ src/components/consultas/atualizarconsulta/AtualizarConsulta.tsx
@@ -107,6 +107,10 @@ export function AtualizarConsultaModal({
 
   if (!isOpen) return null;
 
+  function setValue(arg0: string, medicoSelecionado: Medico) {
+    throw new Error("Function not implemented.");
+  }
+
   return (
     <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
       <div className="bg-white rounded-lg w-full max-w-md p-6 relative">
